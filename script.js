0b03d2622c35404828e9cfde341f768138d034f5
Woodenspoon <wmwarzecha@gmail.com>
2025-01-02 13:08:22 +0100

    add game logic

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const board = function(){
     // generate 3x3 matrix filled with zeros
-    let fields = [...Array(3).keys().map(() => [...Array(3).keys().map(() => null)])]
+    let fields = [...Array(3).keys().map(() => [...Array(3).keys().map(() => null)])];
 
     const setSymbol = function(x, y, symbol){
         fields[y][x] = symbol;
@@ -18,30 +18,33 @@ const board = function(){
         // Check rows
         // row i
         for(let i = 0; i < 3; i++){
-            let col1 = board[i][0];
-            let col2 = board[i][1];
-            let col3 = board[i][2];
-            if(col1 === col2 && col1 === col3){
+            let col1 = fields[i][0];
+            let col2 = fields[i][1];
+            let col3 = fields[i][2];
+            if(col1 && col1 === col2 && col1 === col3){
                 return col1;
             };
         };
         // Check columns
         // col i
         for(let i = 0; i < 3; i++){
-            let row1 = board[0][i];
-            let row2 = board[1][i];
-            let row3 = board[2][i];
-            if(row1 === row2 && row1 === row3){
+            let row1 = fields[0][i];
+            let row2 = fields[1][i];
+            let row3 = fields[2][i];
+            if(row1 && row1 === row2 && row1 === row3){
                 return row1;
             };
         };
-        let middleSymbol = board.getSymbol(1, 1);
+        let middleSymbol = fields[1][1];
+        if(!middleSymbol){
+            return false;
+        }
         // Check right down diagonal
-        if(middleSymbol === board[0][0] && middleSymbol === board.getSymbol[2][2]){
+        if(middleSymbol === fields[0][0] && middleSymbol === fields[2][2]){
             return middleSymbol;
         };
         // Check right up diagonal
-        if(middleSymbol === board.getSymbol[2][0] && middleSymbol === board.getSymbol[0][2]){
+        if(middleSymbol === fields[2][0] && middleSymbol === fields[0][2]){
             return middleSymbol;
         };
         return false;
@@ -49,19 +52,46 @@ const board = function(){
 
     const logBoard = () => console.log(fields);
 
-    return {setSymbol, getSymbol, logBoard, isFull, checkWin};
+    const reset = () => fields.forEach((row) => row.map(() => null));
+
+    return {setSymbol, getSymbol, logBoard, isFull, checkWin, reset};
 }();
 
 const createPlayer = function(name = "guest", symbol){
     const playerName = name;
     const playerSymbol = symbol;
-    return {name, symbol};
+    return {playerName, playerSymbol};
 };
 
 const game = function(){
     let player1 = createPlayer("player1", "o");
     let player2 = createPlayer("player2", "x");
-
+    let player1Move = true;
+    let gameFinished = false;
+    const playRound = function(x, y){
+        if(!gameFinished){
+            if(player1Move){
+                board.setSymbol(x, y, player1.playerSymbol);
+                player1Move = false;
+            }
+            else{
+                board.setSymbol(x, y, player2.playerSymbol);
+                player1Move = true;
+            }
+            checkGameFinish();
+        };
+    };
+    const checkGameFinish = function(){
+        if(board.checkWin()){
+            gameFinished = true;
+            console.log("Player with symbol: ", board.checkWin(), " won");
+        }
+        else if(board.isFull()){
+            console.log("It's a tie");
+            gameFinished = true;
+        }
+    }
+    return {playRound};
 }();
 
 
